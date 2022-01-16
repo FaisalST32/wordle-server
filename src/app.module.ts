012b7690a7852e20d677bf6bd91f55f3db16e6aa
Faisal Rashid <faisalst32@live.com>
2022-01-17 00:51:59 +0530

    testing heroku deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,17 @@ import { GameModule } from './schemas/game.module';
 @Module({
   imports: [
     GameModule,
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING),
+    ConfigModule.forRoot(
+      (() => {
+        return { ignoreEnvFile: false };
+      })(),
+    ),
+    MongooseModule.forRoot(
+      (() => {
+        console.log({ connectionString: process.env.MONGO_CONNECTION_STRING });
+        return process.env.MONGO_CONNECTION_STRING;
+      })(),
+    ),
   ],
   controllers: [],
   providers: [],
